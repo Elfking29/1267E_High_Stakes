fadaff524c3db97afaed6587def35bb6e600c367
Elfking29 <averycoleramsey@gmail.com>
2024-11-01 19:30:05 -0400

    'Ring' Auton added

--- src/autons/ring_auton.cpp
+++ src/autons/ring_auton.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 void ring_auton(int color_value){
-    if (color_value) {
+        move_better_motors(2000,2000,600,5000);
+        move_drive_motors(0,0);
         /*
         // plans for ring_auton: (all values are rough estimates)
         // red version
@@ -64,5 +65,4 @@ void ring_auton(int color_value){
         // drive to ladder?
         */
 
-    }
 }
