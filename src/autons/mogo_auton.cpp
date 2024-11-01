de7d03170c0bccefe0800d762cad3710e2dd07d1
Elfking29 <averycoleramsey@gmail.com>
2024-11-01 19:28:28 -0400

    Adjusting

--- src/autons/mogo_auton.cpp
+++ src/autons/mogo_auton.cpp
@@ -1,24 +1,29 @@
 #include "main.h"
 void mogo_auton(int color_value){
-        move_better_motors(-465,-465,150,1000);
+        move_better_motors(-465,-465,150,1000); //Move Forward
         move_better_motors(color_value*-500,color_value*500,150,750);
-        move_better_motors(-160,-160,150,500);
-        Dunk.move_relative(520,127);
-        delay(750);
-        Dunk.move(-64);
-        delay(1000);
+        //Turn to align with stake
+        move_better_motors(-160,-160,150,500); //Move to stake
+        Dunk.move_relative(520,127); //Dunk ring
+        delay(750); //wait
+        Dunk.move(-64); //Retract dunker
+        delay(1000); //wait
         Dunk.set_brake_mode(E_MOTOR_BRAKE_COAST);
-        Dunk.brake();
-        Ramp.set_brake_mode(E_MOTOR_BRAKE_COAST);
-        move_better_motors(200,200,300,500);
+        Dunk.brake(); //stop dunker
+        move_better_motors(200,200,300,500); //move backwards
         move_better_motors(-825*color_value,825*color_value,200,1000);
+        //turn towards mogo
         move_better_motors(-950,-950,300,1500);
+        //move fast towards mogo
         move_better_motors(-90*color_value,90*color_value,200,300);
+        //get at specific angle
         move_better_motors(-600,-600,127,1200);
-        Clamp.toggle();
-        delay(300);
+        //Move slowly towards mogo
+        Clamp.toggle(); //clamp mogo
+        delay(300); //wait
         move_better_motors(-250*color_value,250*color_value,500,300);
-        move_better_motors(-800,-800,500,1000);
+        //turn towards ladder
+        move_better_motors(-800,-800,500,1000); //move to ladder
 
 
         /*
