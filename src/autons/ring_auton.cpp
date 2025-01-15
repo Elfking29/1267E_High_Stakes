5d26a1994194e63d3686174ad05cdb21cda9e0be
Elfking29 <averycoleramsey@gmail.com>
2025-01-14 21:16:53 -0500

    Auton

--- src/autons/ring_auton.cpp
+++ src/autons/ring_auton.cpp
@@ -1,32 +1,47 @@
 #include "main.h"
 void ring_auton(int color_value){
-	Arm.set_encoder_units(MOTOR_ENCODER_DEGREES);
-    DrivePID drive(0.2,0,0.1,0.4,0,0);
+    DrivePID drive(0.2,0,0.1,15,0,0.4);
     delay(100);
+    drive.prepare(-12);
+    while (!drive.is_finished()){
+        drive.go();
+    }
+    drive.prepare(-90*color_value,1);
+    while (!drive.is_finished()){
+        drive.go();
+    }
+    delay(500);
+    drive.prepare(-3.8);
+    while (!drive.is_finished()){
+        drive.go();
+    }
+    Intake.move_relative(1000,127);
+    delay(1000);
+    Intake.move_relative(-500,64);
+    delay(250);
+    drive.prepare(4);
+    while (!drive.is_finished()){
+        drive.go();
+    }
+    drive.prepare(-135*color_value,1);
+    while (!drive.is_finished()){
+        drive.go();
+    }
+    delay(250);
+    drive.prepare(-30);
+    while (!drive.is_finished()){
+        drive.go();
+    }
+    delay(500);
     drive.prepare(-7);
-    //Ramp.move(127);
     while (!drive.is_finished()){
         drive.go();
-    }
-    drive.prepare(-85,1);
+    }   
+    Clamp.toggle();
+    delay(500);
+    drive.prepare(-145*color_value,1);
     while (!drive.is_finished()){
         drive.go();
-    }
-    delay(1000);
-    Arm.move_absolute(300,100);
-    delay(1000);
-    drive.prepare(-4.5);
-    while (!drive.is_finished()){
-        drive.go();
-    }
-    Arm.move_absolute(0,100);
-    delay(4000);
-    drive.prepare(30);
-    while (!drive.is_finished()){
-        drive.go();
+    }   
     }
     
-
-
-
-}
